494911b80742a79266e90f150a9a36ff4f9b23f6
Austin <austin.cotant@gmail.com>
2018-11-26 16:07:27 -0600

    2 solved

--- 22_names_scores.rb
+++ 22_names_scores.rb
@@ -2,6 +2,7 @@ class NamesScores
   attr_reader :names
   def initialize
     @names = NamesScores.parse_names
+    @names.sort!
   end 
 
   def self.parse_names 
@@ -11,5 +12,21 @@ class NamesScores
     names
   end
 
+  def self.name_score(name) 
+    name.chars.reduce(0) { |acc, char| acc + char.ord - 64 }
+  end 
+
+  def score
+    result = 0
+    names.each_with_index do |name, idx|
+      result += NamesScores.name_score(name) * (idx + 1)
+    end 
+    result
+  end 
+
 end 
+
+n = NamesScores.new 
+puts NamesScores.name_score("COLIN")
+puts n.score
 
