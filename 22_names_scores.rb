a98103811ceb6d1eb9937643e572372e44bda4d8
Austin <austin.cotant@gmail.com>
2018-11-26 14:18:42 -0600

    2 parse the name file

--- 22_names_scores.rb
+++ 22_names_scores.rb
@@ -1,7 +1,15 @@
 class NamesScores
+  attr_reader :names
   def initialize
-    @names = self.parse_names
+    @names = NamesScores.parse_names
   end 
 
-  
+  def self.parse_names 
+    names = File.read("names.txt").split("\",\"")
+    names[0] = names.first[1..-1]
+    names[-1] = names.last[0..-2]
+    names
+  end
+
 end 
+
